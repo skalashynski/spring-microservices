28efac7b1563db5ab3a6c9eafe18ec12fbc384eb
skalashynski <sergei.kalashynski@gmail.com>
2021-07-21 19:46:30 +0300

    Adding discovery server

---
+++ discover-server/src/main/java/com/skalashynski/spring/microservices/discoverserver/Application.java
@@ -0,0 +1,13 @@
+package com.skalashynski.spring.microservices.discoverserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ discover-server/src/test/java/com/skalashynski/spring/microservices/discoverserver/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skalashynski.spring.microservices.discoverserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
