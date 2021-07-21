218fee4aafec8cdb4144158e35dd8292e1416dde
skalashynski <sergei.kalashynski@gmail.com>
2021-07-21 19:54:05 +0300

    Adding discovery server. Adding additional configuration in the pom.xml

--- discover-server/src/main/java/com/skalashynski/spring/microservices/discoverserver/DiscoveryServerApplication.java
+++ discover-server/src/main/java/com/skalashynski/spring/microservices/discoverserver/DiscoveryServerApplication.java
@@ -2,12 +2,14 @@ package com.skalashynski.spring.microservices.discoverserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class Application {
+@EnableEurekaServer
+public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(DiscoveryServerApplication.class, args);
 	}
 
 }
