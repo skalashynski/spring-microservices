5e3b277a0b841f3ecf6d741e96d7ba6a87215206
skalashynski <sergei.kalashynski@gmail.com>
2021-07-22 18:45:51 +0300

    Adding timeout to RestTemplate

--- movie-catalog-service/src/main/java/com/skalashynski/spring/microservices/moviecatalogservice/config/MovieCatalogConfig.java
+++ movie-catalog-service/src/main/java/com/skalashynski/spring/microservices/moviecatalogservice/config/MovieCatalogConfig.java
@@ -3,19 +3,23 @@ package com.skalashynski.spring.microservices.moviecatalogservice.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class MovieCatalogConfig {
+
   @LoadBalanced
   @Bean
   public RestTemplate getRestTemplate() {
-    return new RestTemplate();
+    HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+    factory.setConnectTimeout(3000);
+    return new RestTemplate(factory);
   }
 
   @Bean
-  public WebClient.Builder getWebClientBuilder(){
+  public WebClient.Builder getWebClientBuilder() {
     return WebClient.builder();
   }
 }
--- movie-info-service/src/main/java/com/skalashynski/spring/microservices/movieinfoservice/config/MovieInfoConfig.java
+++ movie-info-service/src/main/java/com/skalashynski/spring/microservices/movieinfoservice/config/MovieInfoConfig.java
@@ -2,6 +2,7 @@ package com.skalashynski.spring.microservices.movieinfoservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,6 +10,8 @@ public class MovieInfoConfig {
 
   @Bean
   public RestTemplate getRestTemplate() {
-    return new RestTemplate();
+    HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+    factory.setConnectTimeout(3000);
+    return new RestTemplate(factory);
   }
 }
